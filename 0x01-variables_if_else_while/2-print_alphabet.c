54c2047633fa8e786e991a100eda7ed6760d09b9
JaneNelson1 <janenelsonadhis@gmail.com>
2022-06-06 16:26:29 -0700

    Alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,11 @@
  */
 int main(void)
 {
-	char low;	
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	        printf("\n")
+ char low;	
+ for (low = 'a'; low <= 'z'; low++)
+{	
+	putchar(low);
+}
+	printf("\n")
 	return (0);
 }
