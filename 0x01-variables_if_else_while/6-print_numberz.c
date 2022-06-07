0def2b139209270d5cd19ba20f5ee25d997da835
JaneNelson1 <janenelsonadhis@gmail.com>
2022-06-06 17:47:06 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - all codes
+ * Description: A program
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int numbers;
+
+for (numbers = 0; numbers < 10; numbers++)
+putchar(numbers + '0');
+
+putchar('\n');
+return (0);
+}
