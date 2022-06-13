87167fd1c9706261ea57679f90118194d1057189
JaneNelson1 <janenelsonadhis@gmail.com>
2022-06-13 13:51:41 -0700

    Fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,9 @@
  */
 void print_rev(char *s)
 {
-	int length;
 	int len;
 
-	for (length = 0; s[length] != '\0'; len++)
+	for (len = 0; s[len] != '\0'; len++)
 	{
 	}
 	for (len = len - 1; len >= 0; len--)
